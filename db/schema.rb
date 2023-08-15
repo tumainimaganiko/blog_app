17e4b6184b8de9631e61cdb7d4b2caaa9425bca6
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-15 19:39:26 +0300

    Create User Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_162715) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_162943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_162715) do
     t.string "Text"
     t.integer "CommentsCounter"
     t.integer "LikesCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "Name"
+    t.string "Photo"
+    t.string "Bio"
+    t.integer "PostsCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
