1bcdfc0fba63d88db6e24713946899891630417c
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-15 22:48:40 +0300

    Add Foreign Keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_194205) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_194640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,5 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_194205) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
