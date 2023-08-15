a7baf2c42b2950acdf5bd7a00ec54c3793de9fcd
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-15 19:26:28 +0300

    Create Posts Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_160855) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_161536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string "Text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "Title"
+    t.string "Text"
+    t.integer "CommentsCounter"
+    t.integer "LikesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
