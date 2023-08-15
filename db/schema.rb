225d7d7333cdd2d87737d1e226a04d30af021478
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-15 22:46:03 +0300

    Add Foreign Key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_193542) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_194205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,4 +53,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_193542) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
 end
