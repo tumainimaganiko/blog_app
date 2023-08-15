a31cd3571ce8e0416d0c2405c6490c2b51ccf1f0
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-15 22:34:06 +0300

    Add AuthorId Column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_185651) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_193318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_185651) do
     t.integer "LikesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
