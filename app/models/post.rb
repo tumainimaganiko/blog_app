f0429866bf7b64085a276b386586eee53f86c2aa
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-17 13:33:34 +0300

    Add Validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true
+  validates :likes_counter, presence: true
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :update_user_posts_counter
 
   def recent_comments(limit = 5)
