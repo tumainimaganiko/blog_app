2c18b9b653c7b0c993d27998cda0daf303b0e6d5
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-17 17:06:38 +0300

    Update Validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,7 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, presence: true
-  validates :likes_counter, presence: true
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_user_posts_counter
 
