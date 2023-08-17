c6a239ae5acf9e89d3f1909d548205d0652b17cd
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-17 11:28:07 +0300

    Set Association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  after_save :update_user_posts_counter
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
+
+  def update_user_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
