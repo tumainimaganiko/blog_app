4a68fedcaa9fbcdfc690363e09f0d1dc0361a6ac
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-17 11:27:42 +0300

    Set Association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_save :update_user_comment_counter
+
+  def update_user_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
