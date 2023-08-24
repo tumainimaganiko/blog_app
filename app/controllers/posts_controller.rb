2a1469f564dd66a3b1e546e1e3eea94806a06fab
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-24 17:31:11 +0300

    Add Show Method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,8 @@ class PostsController < ApplicationController
     @posts = User.find_by(id: params['user_id'])
   end
 
-  
+  def show
+    @posts = User.find_by(id: params['user_id']).posts.find_by(id: params['id'])
+    @comment = Comment.where(post_id: @posts)
+  end
 end
