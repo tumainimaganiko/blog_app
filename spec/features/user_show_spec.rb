fa02e53a61428c2869ee9302d838364528a0fcbc
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-29 22:00:57 +0300

    Create Scenario

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "User Show page", type: :feature do
     before(:each) do
+        # Creating the user
         @user = User.create(
           name: 'Tumaini Maganiko',
           photo: 'https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&w=800',
@@ -12,28 +13,35 @@ RSpec.feature "User Show page", type: :feature do
         @second_post = Post.create(author: @user, title: 'Post, two', text: 'This is my second post')
         @third_post = Post.create(author: @user, title: 'Post, three', text: 'This is my third post')
         @last_post = Post.create(author: @user, title: 'Post four', text: 'This is my fourth post')
-    end
 
-  describe "User's show page" do
-    before(:each) do
+        # Visiting the user's show page
         visit user_path(@user)
     end
 
-    it "should show the user's profile picture" do
+    scenario "should show the user's profile picture" do
       expect(page).to have_css("img[src*='#{@user.photo}']")
     end
 
-    it "I can see the user's username" do
+    scenario "I can see the user's username" do
         expect(page).to have_content('Tumaini Maganiko')
     end
 
-    it "I can see the number of posts the user has written" do
+    scenario "I can see the number of posts the user has wrscenarioten" do
       expect(page).to have_content("Number of posts: 4")
     end
 
-    it "I can see the user's bio" do
+    scenario "I can see the user's bio" do
       expect(page).to have_content("I am a great programmer")
     end
 
-  end
+    scenario "I can see the user's first 3 posts." do
+        recent_posts = @user.recent_posts
+        recent_posts.each do |post|
+          expect(page).to have_content(post.title)
+          expect(page).to have_content(post.text)
+          expect(page).to have_content("comments: #{post.comments_counter}")
+          expect(page).to have_content("Likes: #{post.likes_counter}")
+        end
+    end
+
 end
