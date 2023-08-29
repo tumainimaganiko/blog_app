7487ca35b590c5de369d8336df6ec07f04d73946
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-29 20:46:17 +0300

    Update Scenarios

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "User Index Page", type: :feature do
     before(:each) do
+      # Creating the first User
         @first_user = User.create(
           name: 'Tumaini Maganiko',
           photo: 'https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&w=800',
@@ -13,6 +14,7 @@ RSpec.feature "User Index Page", type: :feature do
         @third_post = Post.create(author: @first_user, title: 'Post, three', text: 'This is my third post')
         @last_post = Post.create(author: @first_user, title: 'Post four', text: 'This is my fourth post')
 
+      # Creating the Second User
         @second_user = User.create(
           name: 'Marco Diaz',
           photo: 'https://images.pexels.com/photos/326212/pexels-photo-326212.jpeg?auto=compress&cs=tinysrgb&w=800',
@@ -22,31 +24,34 @@ RSpec.feature "User Index Page", type: :feature do
         @first_post = Post.create(author: @second_user, title: 'Post, one', text: 'This is my first post')
         @second_post = Post.create(author: @second_user, title: 'Post, two', text: 'This is my second post')
         @third_post = Post.create(author: @second_user, title: 'Post, three', text: 'This is my third post')
-    end
+
+        # Visiting the index page
+        visit users_path
+  end
 
   scenario "I can see the username of all other users" do
-    visit users_path
       expect(page).to have_content('Tumaini Maganiko')
       expect(page).to have_content('Marco Diaz')
   end
 
   scenario "I can see the profile picture for each user" do
-    visit users_path
       expect(page).to have_css("img[src*='#{@first_user.photo}']")
       expect(page).to have_css("img[src*='#{@second_user.photo}']")
   end
 
   scenario "I can see the number of posts each user has written" do
-    visit users_path
       expect(page).to have_content("Number of posts: 4")
       expect(page).to have_content("Number of posts: 3")
   end
 
-  scenario "When I click on a user, I am redirected to that user's show page" do
-    visit users_path  
+  scenario "When I click on the first user, I am redirected to that user's show page" do
     click_link @first_user.name
-
     expect(current_path).to eq(user_path(@first_user))
+  end
+
+  scenario "When I click  the second user, I am redirected to that user's show page" do
+    click_link @second_user.name
+    expect(current_path).to eq(user_path(@second_user))
   end
 
 end
