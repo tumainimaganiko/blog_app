164213ef5d7c6ac94b81205a2f1f9da8ce8ffcff
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-29 19:24:47 +0300

    Update Bio

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "User Index Page", type: :feature do
         @first_user = User.create(
           name: 'Tumaini Maganiko',
           photo: 'https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&w=800',
-          bio: 'Teacher from Mexico.',
+          bio: 'I am a great programmer',
           posts_counter: 0
         )
         @first_post = Post.create(author: @first_user, title: 'Post One', text: 'This is my first post')
@@ -16,7 +16,7 @@ RSpec.feature "User Index Page", type: :feature do
         @second_user = User.create(
           name: 'Marco Diaz',
           photo: 'https://images.pexels.com/photos/326212/pexels-photo-326212.jpeg?auto=compress&cs=tinysrgb&w=800',
-          bio: 'Teacher from Mexico.',
+          bio: 'I am a great programmer',
           posts_counter: 0
         )
         @first_post = Post.create(author: @second_user, title: 'Post, one', text: 'This is my first post')
