4e0154752eab2d994dafe0fa5637b671c7ae6ac7
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-30 23:58:23 +0300

    Fix Linters

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -33,15 +33,24 @@ RSpec.feature 'User Show page', type: :feature do
   scenario "I can see the user's bio" do
     expect(page).to have_content(@user.bio)
   end
+end
 
-  scenario "I can see the user's first 3 posts." do
-    recent_posts = @user.recent_posts
-    recent_posts.each do |post|
-      expect(page).to have_content(post.title)
-      expect(page).to have_content(post.text)
-      expect(page).to have_content("comments: #{post.comments_counter}")
-      expect(page).to have_content("Likes: #{post.likes_counter}")
-    end
+RSpec.feature 'User Show page', type: :feature do
+  before do
+    # Creating the user
+    @user = User.create(
+      name: 'Tumaini Maganiko',
+      photo: 'https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&w=800',
+      bio: 'I am a great programmer',
+      posts_counter: 0
+    )
+    @first_post = Post.create(author: @user, title: 'Post One', text: 'This is my first post')
+    @second_post = Post.create(author: @user, title: 'Post, two', text: 'This is my second post')
+    @third_post = Post.create(author: @user, title: 'Post, three', text: 'This is my third post')
+    @last_post = Post.create(author: @user, title: 'Post four', text: 'This is my fourth post')
+
+    # Visiting the user's show page
+    visit user_path(@user)
   end
 
   scenario "I can see a button that lets me view all of a user's posts" do
@@ -51,5 +60,15 @@ RSpec.feature 'User Show page', type: :feature do
   scenario "When I click to see all posts, it redirects me to the user's post's index page" do
     click_link('See all posts')
     expect(page.current_path).to eq(user_posts_path(@user))
+  end
+
+  scenario "I can see the user's first 3 posts." do
+    recent_posts = @user.recent_posts
+    recent_posts.each do |post|
+      expect(page).to have_content(post.title)
+      expect(page).to have_content(post.text)
+      expect(page).to have_content("comments: #{post.comments_counter}")
+      expect(page).to have_content("Likes: #{post.likes_counter}")
+    end
   end
 end
