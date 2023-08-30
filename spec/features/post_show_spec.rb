4fb366ba426323fa31c492454021605aa76b0dca
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-30 09:39:15 -0600

    Create file post_show_spec

---
+++ spec/features/post_show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+RSpec.feature 'Post show page', type: :feature do
+  before do
+    @user = User.create(
+        name: 'Tumaini Maganiko',
+        photo: 'https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&w=800',
+        bio: 'I am a great programmer',
+        posts_counter: 0
+      )
+      @first_post = Post.create(author: @user, title: 'Post One', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+      @second_post = Post.create(author: @user, title: 'Post, two', text: 'This is my second post', comments_counter: 0, likes_counter: 0)
+      @comment1 = Comment.create(author: @user, post: @first_post, text: 'comment 1')
+      @comment2 = Comment.create(author: @user, post: @first_post, text: 'comment 2')
+      visit user_post_path(@user, @first_post)
+  end
+  
