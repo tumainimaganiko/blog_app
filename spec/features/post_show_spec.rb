768cabf45ce889ec211bea0fd7f74539cb7d21e3
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-30 23:56:28 +0300

    Fix Linters

--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
 RSpec.feature 'Post show page', type: :feature do
   before do
-    @user = User.create(
-      name: 'Tumaini Maganiko',
-      photo: 'https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&w=800',
-      bio: 'I am a great programmer',
-      posts_counter: 0
-    )
+    @user = User.create(name: 'Tumaini Maganiko', photo: 'https://image.com/image.jpg', bio: 'I am a great programmer',
+                        posts_counter: 0)
     @first_post = Post.create(author: @user, title: 'Post One', text: 'This is my first post', comments_counter: 0,
                               likes_counter: 0)
     @second_post = Post.create(author: @user, title: 'Post, two', text: 'This is my second post',
@@ -34,6 +30,20 @@ RSpec.feature 'Post show page', type: :feature do
 
   scenario 'I can see the post body' do
     expect(page).to have_content(@first_post.text)
+  end
+end
+
+RSpec.feature 'Post show page', type: :feature do
+  before do
+    @user = User.create(name: 'Tumaini Maganiko', photo: 'https://image.com/image.jpg', bio: 'I am a great programmer',
+                        posts_counter: 0)
+    @first_post = Post.create(author: @user, title: 'Post One', text: 'This is my first post', comments_counter: 0,
+                              likes_counter: 0)
+    @second_post = Post.create(author: @user, title: 'Post, two', text: 'This is my second post',
+                               comments_counter: 0, likes_counter: 0)
+    @comment1 = Comment.create(author: @user, post: @first_post, text: 'comment 1')
+    @comment2 = Comment.create(author: @user, post: @first_post, text: 'comment 2')
+    visit user_post_path(@user, @first_post)
   end
 
   scenario 'I can see the username of each commentor' do
